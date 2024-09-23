09698ac2d1346f408eee0d0e121fdd4adf5f31ce
Aykut Bozkurt <aykut.bozkurt1995@gmail.com>
2024-09-23 13:31:36 +0300

    Reads Arrow arrays from Parquet file

--- src/arrow_parquet.rs
+++ src/arrow_parquet.rs
@@ -1,6 +1,7 @@
 pub(crate) mod arrow_to_pg;
 pub(crate) mod arrow_utils;
 pub(crate) mod codec;
+pub(crate) mod parquet_reader;
 pub(crate) mod parquet_writer;
 pub(crate) mod pg_to_arrow;
 pub(crate) mod schema_visitor;
